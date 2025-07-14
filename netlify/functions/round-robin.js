f7c8956957154d1f8556080ed2b7f3cc799038b5
FelipinhoM <kikomiliossi@gmail.com>
2025-07-14 10:10:38 -0300

    Update round-robin.js

--- netlify/functions/round-robin.js
+++ netlify/functions/round-robin.js
@@ -3,9 +3,11 @@
 let counter = 0; // reinicia a cada deploy/restart; para produção, usar Redis ou KV
 
 const attendants = [
-  "https://wa.me/5544988043222",
-  "https://wa.me/5535999543246",
-  "https://wa.me/5537933002836"
+  "https://wa.me/5522981307064",
+  "https://wa.me/5522981087361",
+  "https://wa.me/5522981276734",
+  "https://wa.me/5522981276824",
+  "https://wa.me/5522981499458",
 ];
 
 exports.handler = async function(event, context) {
