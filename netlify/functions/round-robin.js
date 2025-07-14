a1966167d443fbd07ececbd479b30a6dda3064fd
FelipinhoM <kikomiliossi@gmail.com>
2025-07-14 10:26:07 -0300

    Update round-robin.js

--- netlify/functions/round-robin.js
+++ netlify/functions/round-robin.js
@@ -2,12 +2,14 @@
 
 let counter = 0; // reinicia a cada deploy/restart; para produção, usar Redis ou KV
 
+const message = encodeURIComponent("Olá Arlênio, quero aprender a te imitar e ganhar 10 mil ainda neste mês.");
+
 const attendants = [
-  "https://wa.me/5522981307064",
-  "https://wa.me/5522981087361",
-  "https://wa.me/5522981276734",
-  "https://wa.me/5522981276824",
-  "https://wa.me/5522981499458",
+  `https://wa.me/5522981307064?text=${message}`,
+  `https://wa.me/5522981087361?text=${message}`,
+  `https://wa.me/5522981276734?text=${message}`,
+  `https://wa.me/5522981276824?text=${message}`,
+  `https://wa.me/5522981499458?text=${message}`
 ];
 
 exports.handler = async function(event, context) {
