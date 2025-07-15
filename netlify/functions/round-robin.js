34186b0e251f65b62e96736a1553819983e60404
FelipinhoM <kikomiliossi@gmail.com>
2025-07-15 13:10:40 -0300

    Update round-robin.js influ2

--- netlify/functions/round-robin.js
+++ netlify/functions/round-robin.js
@@ -5,9 +5,6 @@ let counter = 0; // reinicia a cada deploy/restart; para produção, usar Redis 
 const message = encodeURIComponent("Olá Arlênio, quero aprender a te imitar e ganhar 10 mil ainda neste mês.");
 
 const attendants = [
-  `https://wa.me/5522981307064?text=${message}`,
-  `https://wa.me/5522981087361?text=${message}`,
-  `https://wa.me/5522981276734?text=${message}`,
   `https://wa.me/5522981276824?text=${message}`,
   `https://wa.me/5522981499458?text=${message}`
 ];
